cc0b5ace2b20d998dbdafc03954cb6815f642b30
LameckMeshack <otienolameck327@gmail.com>
2024-01-19 22:43:48 +0300

    feat(app): Connected to MongoDB atlass

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const color = require("colors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 3000;
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL).then(()=>console.log('DB connection successful!'.bgGreen)).catch((err)=>console.log(err));
+
+
 
 app.get("/", (req, res) => res.send("Furniture world!"));
 app.listen(port, () =>
   console.log(`Example app listening on port ${port}!`.blue)
 );
+
+ 
+// mongodb+srv://Rhino:<password>@cluster0.iqwmz9u.mongodb.net/
