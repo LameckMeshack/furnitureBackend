1720dcfd660218d0edaa80208acdd02673fa9e88
LameckMeshack <otienolameck327@gmail.com>
2024-01-26 06:15:49 +0300

    feat(app): Created Order Models
    Added order and Cart models

---
+++ models/Order.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const OrderSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    customerId: {
+      type: String,
+      required: true,
+    },
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    subtotal: {
+      type: Number,
+      required: true,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+    delivery_status: {
+      type: String,
+      default: "pending",
+    },
+    payment_status: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Order", OrderSchema);
