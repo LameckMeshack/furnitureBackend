ea95db69de697dc20bc4c3e814674956f1f93412
LameckMeshack <otienolameck327@gmail.com>
2024-01-31 07:31:15 +0300

    feat(app): Add to cart controller method

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,12 +1,40 @@
 const Product = require("../models/Products");
 const Cart = require("../models/Cart");
+const { response } = require("express");
 
 module.exports = {
   addToCart: async (req, res) => {
     const { userId, cartItem, quantity } = req.body;
     try {
-        const cart = await Cart.findOne({userId})
-        //TO D0
+      const cart = await Cart.findOne({ userId });
+
+      if (cart) {
+        const existingProduct = cart.products.find(
+          (product) => product.cartItem.toString() === cartItem
+        );
+
+        if (existingProduct) {
+          existingProduct.quantity + 1;
+        } else {
+          cart.products.push({ cartItem, quantity });
+        }
+
+        await cart.save();
+        return response.status(200).json("Product added to cart");
+      } else {
+        const newCart = new Cart({
+          userId,
+          products: [
+            {
+              cartItem,
+              quantity: quantity,
+            },
+          ],
+        });
+
+        await newCart.save();
+        return res.status(200).json("Product added to cart");
+      }
     } catch (error) {}
   },
   getCart: async (req, res) => {},
